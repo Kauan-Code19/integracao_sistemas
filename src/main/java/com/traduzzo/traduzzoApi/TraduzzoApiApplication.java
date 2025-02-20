ecc153802f55baa89fab6fbd3ce4f0b704b0c61c
Kauan-Code19 <pkauprofissional@gmail.com>
2025-02-19 23:44:58 -0300

    adicionando .gitignore e attributes, TraduzzoApiApplication.java, TraduzzoApiApplicationTest.java

---
+++ src/main/java/com/traduzzo/traduzzoApi/TraduzzoApiApplication.java
@@ -0,0 +1,13 @@
+package com.traduzzo.traduzzoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraduzzoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraduzzoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traduzzo/traduzzoApi/TraduzzoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traduzzo.traduzzoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraduzzoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
